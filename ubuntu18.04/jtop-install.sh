65135ec7779c9f5688c0d17b582969dcbf644656
John Chen <cp_china@163.com>
2021-01-03 16:42:39 +0800

    Renamed jtop-install.sh

---
+++ ubuntu18.04/jtop-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install git cmake
+sudo apt-get install python3-dev
+sudo apt-get install libhdf5-serial-dev hdf5-tools
+sudo apt-get install libatlas-base-dev gfortran
+
+sudo apt-get install python3-pip
+sudo pip3 install jetson-stats
