dad39f8ef95a8140fe5482cda42961c790d86d6d
Johnthan Chen <cp_china@163.com>
2021-01-05 13:18:46 +0800

    add nano 2g setup shell

---
+++ ubuntu18.04/01_jetson-nano-2g_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker pull nvcr.io/nvidia/l4t-base:r32.4.4
+
+# Allow containers to communicate with Xorg
+sudo xhost +si:localuser:root
+sudo docker run -it --rm --net=host --runtime nvidia  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix nvcr.io/nvidia/l4t-base:r32.4.4
+# sudo docker run --runtime nvidia --network host -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix nvcr.io/nvidia/l4t-base:r32.3.1
+
+# root@nano:/# apt-get update && apt-get install -y --no-install-recommends make g++
+# root@nano:/# cp -r /usr/local/cuda/samples /tmp
+# root@nano:/# cd /tmp/samples/5_Simulations/nbody
+# root@nano:/# make
+# root@nano:/# ./nbody
+
+
+# https://blog.csdn.net/liang890319/article/details/109495984
---
+++ ubuntu18.04/02_jetson-nano-2g_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# IP地址
+ifconfig
+
+
+
+# 查看内核版本
+cat /proc/version && uname -a && lsb_release -a
+
+# 查看gnome version
+gnome-shell --version
+sudo gdm3 --version
