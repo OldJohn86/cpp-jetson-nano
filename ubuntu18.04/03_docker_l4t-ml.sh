3e1a037d3463ab33aeb3279c497452a5b49523a2
cpp_china <cp_china@163.com>
2021-01-07 20:21:33 +0800

    update 03_docker_l4t-ml

--- ubuntu18.04/03_docker_l4t-ml.sh
+++ ubuntu18.04/03_docker_l4t-ml.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # https://ngc.nvidia.com/catalog/containers/nvidia:l4t-tensorflow
+
 docker pull nvcr.io/nvidia/l4t-ml:r32.4.4-py3
 
-# Allow containers to communicate with Xorg
-# sudo xhost +si:localuser:root
-# sudo docker run -it --rm --net=host --runtime nvidia  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix nvcr.io/nvidia/l4t-base:r32.4.4
-# sudo docker run --runtime nvidia --network host -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix nvcr.io/nvidia/l4t-base:r32.3.1
+# Have a look
+docker image ls
 
-# root@nano:/# apt-get update && apt-get install -y --no-install-recommends make g++
-# root@nano:/# cp -r /usr/local/cuda/samples /tmp
-# root@nano:/# cd /tmp/samples/5_Simulations/nbody
-# root@nano:/# make
-# root@nano:/# ./nbody
+# YOU YAO GUA ZAI DE MU LU(/home/cpp/nvidia-ml)
+sudo docker run -itd --name nvidiaml --runtime  nvidia --network host -v /home/cpp/nvidia-ml:/home/cpp/nvidia-ml nvcr.io/nvidia/l4t-ml:r32.4.4-py3
 
-# https://blog.csdn.net/liang890319/article/details/110257526
+# Check status
+docker ps -a
+
+# Start the container
+docker start nvidiaml
+
+# Access to the container
+docker exec -it nvidiaml bash
+
